769fc59e42a99b16f6a374b6911fc78dfe816389
Kevin Dungs <kevin@dun.gs>
2014-10-28 10:29:26 +0100

    A few fixes. Also changed strucure and naming in json a bit.

--- js/craft.js
+++ js/craft.js
@@ -3,7 +3,12 @@
 var Craft = (function() {
   'use strict';
 
-  var measurements = ['Tracking', 'ECal'];
+  // Temporary definitions of objects that will later be loaded dynamically
+  // from JSON.
+  var components = ['tracker', 'ecal', 'hcal', 'mcal'];
+  var measurements = ['track', 'energy'];
+  var particles = ['electron'];
+  // --------------------------------------------------------------------------
 
   var Detector = function() {
     this.components = [];
@@ -30,9 +35,9 @@ var Craft = (function() {
       var comp = this.components[i];
       var meas = comp.measurements[particleId];
       measurements.forEach(function(m) {
-        performance[m] *= 1.0 - meas[m];
+        performance[m] *= 1.0 - (meas[m] || 0.0);
       });
-      if (comp.stops.indexOf(particleId) > -1) {
+      if (comp.stops && comp.stops.indexOf(particleId) > -1) {
         break;
       };
     }
